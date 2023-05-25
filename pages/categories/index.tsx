5f4630fbb3263451d755f3f50b80844aea4d8492
Valor-mmm <v_dev/github@slmail.me>
2023-05-25 21:14:54 +0200

    Add initial project structure

---
+++ pages/categories/index.tsx
@@ -0,0 +1,19 @@
+import { MuiListInferencer } from "@refinedev/inferencer/mui";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export default function CategoryList() {
+  return <MuiListInferencer />;
+}
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
+};
