307e6e58d489c3c19c415e13b8d673af2fa5949e
Valor-mmm <v_dev/github@slmail.me>
2023-05-26 23:47:31 +0200

    Add ability to redirect filters

--- src/app/mapPage/page.tsx
+++ src/app/mapPage/page.tsx
@@ -1,12 +1,23 @@
 "use client";
 
-import { FormControl, Stack, TextField, Box, InputAdornment} from "@mui/material";
+import {
+  FormControl,
+  Stack,
+  TextField,
+  Grid,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
 import Map from "../../components/maps";
 import { AddressAutofill } from "@mapbox/search-js-react";
 import { useState } from "react";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+
+import { useRouter } from "next/navigation";
 
 export default function MapPage() {
+  const router = useRouter();
   const [location, setLocation] = useState<{ lat: number; lng: number }>({
     lat: 55.705511,
     lng: 12.554729,
@@ -14,35 +25,68 @@ export default function MapPage() {
 
   return (
     <main style={{ width: "100%", height: "100%", padding: "0 8px 8px 8px" }}>
-      <h1 style={{marginLeft: "20px"}}>Map</h1>
-      <Stack width="100%" height="100%" display="flex" alignItems="center" spacing={4}>
-        <form style={{width: "80%"}} onSubmit={(evt) => {evt.preventDefault()}}>
-          <FormControl fullWidth>
-            <AddressAutofill
-              onRetrieve={(result) => {
-                const coordinates = result.features[0].geometry.coordinates
-                setLocation({lng: coordinates[0], lat: coordinates[1]})
+      <h1 style={{ marginLeft: "20px" }}>Map</h1>
+      <Stack
+        width="100%"
+        height="100%"
+        display="flex"
+        alignItems="center"
+        spacing={3}
+      >
+        <Grid container style={{ width: "80%" }} spacing={2}>
+          <Grid item xs={8} md={7} style={{ paddingLeft: "9px" }}>
+            <form
+              onSubmit={(evt) => {
+                evt.preventDefault();
               }}
-              accessToken={process.env.NEXT_PUBLIC_AUTOCOMPLETE_TOKEN as string}
             >
-              <TextField
-                fullWidth
-                style={{ maxWidth: "70%"}}
-                placeholder="Tell us where we should search for you"
-                autoComplete="address-line1"
-                InputProps={{
-                  style: {fontSize: "18px", fontWeight: "bold", padding: "8px"},
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      < SearchIcon/>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </AddressAutofill>
-          </FormControl>
-        </form>
-          <Map searchLocation={location} />
+              <FormControl fullWidth>
+                <AddressAutofill
+                  onRetrieve={(result) => {
+                    const coordinates = result.features[0].geometry.coordinates;
+                    setLocation({ lng: coordinates[0], lat: coordinates[1] });
+                  }}
+                  accessToken={
+                    process.env.NEXT_PUBLIC_AUTOCOMPLETE_TOKEN as string
+                  }
+                >
+                  <TextField
+                    fullWidth
+                    placeholder="Tell us where we should search for you"
+                    autoComplete="address-line1"
+                    InputProps={{
+                      style: {
+                        fontSize: "18px",
+                        padding: "8px",
+                      },
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <SearchIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </AddressAutofill>
+              </FormControl>
+            </form>
+          </Grid>
+          <Grid item xs={4} md={2} display="flex" justify-content="flex-start">
+            <IconButton
+              onClick={() => {
+                router.push("filters");
+              }}
+              size="large"
+              style={{
+                borderRadius: "5px",
+                backgroundColor: "#3E966D",
+                color: "white",
+              }}
+            >
+              <FilterAltIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+        <Map searchLocation={location} />
       </Stack>
     </main>
   );
