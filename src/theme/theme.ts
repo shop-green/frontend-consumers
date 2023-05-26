edeaef33e280e069a57b2ccde65f61e5e538749f
Valor-mmm <v_dev/github@slmail.me>
2023-05-26 22:24:09 +0200

    Add theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,3 +1,25 @@
 import createTheme from "@mui/material/styles/createTheme";
 
-export const theme = createTheme({});
+export const theme = createTheme({
+    palette: {
+      primary: {
+        light: "#D8EAE2",
+        main: "#3E966D",
+        dark: "#275E44",
+      },
+      secondary: {
+        main: "#F0DBC5",
+        light: "#F0DBC5",
+        dark: "#D9A56F",
+      },
+      grey: {
+        300: "#E4E4E4",
+        500: "#C9C8C8",
+        800: "#454343",
+      },
+      background: {
+        default: "#F0F2F8",
+      }
+    },
+  });
+  
